60234e6e7bf5f6f7e1b7d2cfe2a3d6b083c33af5
Mindaugas Tamosiunas <mindaugastamosiunas.1992@gmail.com>
2021-03-22 19:25:41 +0200

    Test

---
+++ Kursai_2021-03-22/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Kursai_2021_03_22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine();
+            Console.WriteLine("------- Press any key to continue -------");
+            Console.ReadKey();
+        }
+    }
+}
