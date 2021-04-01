561a84d3cda329f482b72ab5f4fb5a2dd83c63e9
Mindaugas Tamosiunas <mindaugastamosiunas.1992@gmail.com>
2021-04-01 18:01:31 +0300

    P5

---
+++ P5_OOP/P5_OOP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P5_OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
